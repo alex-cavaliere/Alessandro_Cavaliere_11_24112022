e1fd374a1adb838462e35e5e12e0e936260a0a1d
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-12-07 14:16:17 +0100

    carousel add

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -4,26 +4,35 @@ import chevronRight from '../../assets/images/chevron-right.svg'
 import {useState} from 'react'
 
 function Carousel(props){
-    const [imgIndex, setImgIndex] = useState(0)
+    const [imgIndex, setImgIndex] = useState(1)
     const {pictures} = props
     const [firstPic, setFirstPic] = useState(pictures[0])
     const nextPic = () => {
+        setImgIndex(imgIndex + 1)
         if(imgIndex < pictures.length){
-            setImgIndex(imgIndex + 1)
             setFirstPic(pictures[imgIndex])
         }else{
-            setImgIndex(0)
+            setImgIndex(1)
             setFirstPic(pictures[0])
         }
+        console.log(imgIndex)
+    }
+    const prevPic = () => {
+        setImgIndex(imgIndex - 1)
+        if(imgIndex === 1){
+            setImgIndex(pictures.length)
+        }
+        console.log(imgIndex)
+        setFirstPic(pictures[imgIndex - 1])
     }
     return(
         <div className="carousel-container">
             <div className="carousel">
                 <img src={firstPic}/>
-                <div className='carousel-controls'>
-                    <img src={chevronLeft} alt='chevronLeft'></img>
+                {pictures.length > 1 && <div className='carousel-controls'>
+                    <img src={chevronLeft} alt='chevronLeft' onClick={prevPic}></img>
                     <img src={chevronRight} alt='chevronRight' onClick={nextPic}></img>
-                </div>
+                </div>}
             </div>
         </div>
     )
