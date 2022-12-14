ac09c592b1ad3de3885242b98701adec8662a549
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-12-14 11:50:33 +0100

    finalisation Error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
 function Error(){
     return(
-        <div>
-            <h1>Error 404</h1>
+        <div className='error'>
+            <h1>404</h1>
+            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            <p>
+                <Link to="/">Retourner sur la page d’accueil</Link>
+            </p>
         </div>
     )
 }
